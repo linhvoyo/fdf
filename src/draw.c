9e9d4a3705a61601e27eb363cbdf61b38803db1d
linhvoyo <linhvoyo@ymail.com>
2018-03-27 21:12:29 -0700

    done with bresenham algor

--- src/draw.c
+++ src/draw.c
@@ -355,161 +355,295 @@ float y_coor(t_mlx *mlx, int i)
 //     y = HEIGHT / 2 + y;
 // }
 
-void render(t_mlx *mlx)
+void transform(t_mlx *mlx, float *coord, int i)
 {
     float x;
     float y;
     float z;
     float theta;
+
+    x = i % mlx->map->width;
+    y = i / mlx->map->width;
+    z = mlx->map->coords[i][0];
+
+    //scale image
+    x = x * mlx->map->scale_factor;
+    y = y * mlx->map->scale_factor;
+    z = z * mlx->map->scale_factor;
+
+    //rotate_x;
+    theta = degrees_to_radians(90);
+    y = y * cos(theta) - z * sin(theta);
+    z = y * cos(theta) + z * sin(theta);
+
+    //center
+    x = x + WIDTH / 2 - (mlx->map->width * mlx->map->scale_factor) /2;
+    y = HEIGHT / 2 + y;
+
+    coord[0] = x;
+    coord[1] = y;
+    coord[2] = z;
+
+}
+
+
+// if (row < map->width && y < map->height)
+// {
+//     printf("x: %f y: %f \n", x, y);
+//     printf("x2: %f y2: %f \n", x , y + 1);
+//     row++;
+// }
+// else if (row == map->width)
+// {
+//     printf("\n");
+//     row = 0;
+//     i--;
+// }
+
+void render(t_mlx *mlx)
+{
+    float *coord_current;
+    float *coord_next;
     int i;
     int row;
     int column;
 
-    clear_image(mlx->img);
-    printf("render scale_factor: %f\n", mlx->map->scale_factor);
+    coord_current = malloc(sizeof(float) * 4);
+    coord_next = malloc(sizeof(float) * 4);
+
     i = 0;
     row = 0;
     column = 0;
-
-    float y_column;
-
-    float x_below;
-    float y_below;
-    float x2;
-    float y2;
-    // mlx->map->scale_factor = 1;
+    clear_image(mlx->img);
     while (i < mlx->map->width * mlx->map->height)
     {
-        // x = i % mlx->map->width;
-        // y = i / mlx->map->width;
-        // z = mlx->map->coords[i][0];
-        // y_column = y + 1;
 
-        // //scale image
-        // x = x * mlx->map->scale_factor;
-        // y = y * mlx->map->scale_factor;
-        // z = z * mlx->map->scale_factor;
-        // y_column = y_column * mlx->map->scale_factor;
+        transform(mlx, coord_current, i);
+        plot_pixel(mlx, coord_current[0], coord_current[1]);
 
-
-
-        //draw_line(mlx, x_coor(mlx, i), y_coor(mlx, i), x_coor(mlx, i + 1), y_coor(mlx, i + 1));
-
-
-
-        x = x_coor(mlx, i);
-        y = y_coor(mlx, i);
-        x2 = x_coor(mlx, i + 1);
-        y2 = y_coor(mlx, i + 1);
-        x_below = x_coor(mlx, i + mlx->map->width);
-        y_below = y_coor(mlx, i + mlx->map->width);
-
-
-        if (row < mlx->map->width && column < mlx->map->height)
+        if ((row < mlx->map->width) && (column < mlx->map->height))
         {
             if (row != mlx->map->width - 1)
             {
-                // draw_line(mlx, x_coor(mlx, i), y_coor(mlx, i), x_coor(mlx, i + 1), y_coor(mlx, i + 1));
-
-                //rotate_x;
-                theta = degrees_to_radians(90);
-                y = y * cos(theta) - z_coor(mlx, i) * sin(theta);
-                // z = y * cos(theta) + z * sin(theta);
-                y2 = y2 * cos(theta) - z_coor(mlx,i + 1) * sin(theta);
-
-                draw_line(mlx, x, y, x2, y2);
-                // if (column != mlx->map->height - 1)
-                //     draw_line(mlx, x_coor(mlx, i), y_coor(mlx, i), x_coor(mlx, i + mlx->map->width), y_coor(mlx, i + mlx->map->width));
-                printf("x: %f y: %f \n", x_coor(mlx, i), y_coor(mlx,i));
+                transform(mlx, coord_next, i + 1);
+                draw_line(mlx, coord_current[0], coord_current[1], coord_next[0], coord_next[1]);
+                printf("x: %f y: %f \n", coord_current[0], coord_current[1]);
             }
-            if (column != mlx->map->height - 1)
+            if ((row != mlx->map->width) && (column != mlx->map->height - 1))
             {
-                y = y * cos(theta) - z_coor(mlx, i) * sin(theta);
-                y_below = y_below * cos(theta) - z_coor(mlx,i + mlx->map->width) * sin(theta);
-                draw_line(mlx, x, y, x_below, y_below);
+                transform(mlx, coord_next, i + mlx->map->width);
+                draw_line(mlx, coord_current[0], coord_current[1], coord_next[0], coord_next[1]);
+                printf("x: %f y: %f \n", coord_next[0], coord_next[1]);
             }
-                // draw_line(mlx, x_coor(mlx, i), y_coor(mlx, i), x_coor(mlx, i + mlx->map->width), y_coor(mlx, i + mlx->map->width));
-            // printf("x2: %f y2: %f \n", x , y + 1);
             row++;
         }
-        else if (row == mlx->map->width)
+        if (row == mlx->map->width)
         {
-            printf("\n");
+            printf("zero\n");
             row = 0;
             column++;
-            printf("%d\n", column);
-            i--;
         }
-
-
-
-
-        // float x_prev;
-        // float y_prev;
-        // if ((row < mlx->map->width - 0) && y < mlx->map->height)
-        // {
-        //     //scale image
-        //     x = x * mlx->map->scale_factor;
-        //     y = y * mlx->map->scale_factor;
-        //     z = z * mlx->map->scale_factor;
-        //     y_column = y_column * mlx->map->scale_factor;
-        //
-        //     // //rotate_x;
-        //     // theta = degrees_to_radians(90);
-        //     // y = y * cos(theta) - z * sin(theta);
-        //     // z = y * cos(theta) + z * sin(theta);
-        //     // y_column = y_column * cos(theta) - z * sin(theta);
-        //
-        //     x = x + WIDTH / 2 - (mlx->map->width * mlx->map->scale_factor) /2;
-        //     y = HEIGHT / 2 + y;
-        //     y_column = HEIGHT / 2 + y_column;
-        //
-        //     // draw_line(mlx, x_coor(mlx, i), y_coor(mlx, i), x_coor(mlx, i + 1), y_coor(mlx, i + 1));
-        //     // printf("x: %f y: %f x_column: %f y_column: %f\n", x, y, x, y_column);
-        //
-        //     if (row > 1)
-        //     {
-        //         draw_line(mlx, x_prev, y_prev, x , y);
-        //         draw_line(mlx, x, y, x, y_column);
-        //         printf("x: %f y: %f x_column: %f y_column: %f\n", x, y, x, y_column);
-        //         x_prev = x;
-        //         y_prev = y;
-        //     }
-        //     else if (row == 0)
-        //     {
-        //         x_prev = x;
-        //         y_prev = y;
-        //     }
-        //     // printf("x: %f y: %f z: %f\n", x, y, z);
-        //     // plot_pixel(mlx,x,y);
-        //     row++;
-        // }
-        // if ((row == mlx->map->width) && (row = 0))
-        //     i = i - 2;
-
-
-        //
-        // //scale image
-        // x = x * mlx->map->scale_factor;
-        // y = y * mlx->map->scale_factor;
-        // z = z * mlx->map->scale_factor;
-        //
-        // //rotate_x;
-        // theta = degrees_to_radians(90);
-        // y = y * cos(theta) - z * sin(theta);
-        // z = y * cos(theta) + z * sin(theta);
-        //
-        // //center
-        // x = x + WIDTH / 2 - (mlx->map->width * mlx->map->scale_factor) /2;
-        // y = HEIGHT / 2 + y;
-        // // printf("x:%f y:%f ", x, y);
-        // plot_pixel(mlx, x, y);
-
         i++;
-        printf("\n");
     }
+    free(coord_current);
+    free(coord_next);
     mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, mlx->img->img, 0 , 0);
 }
+//
+
+// void render(t_mlx *mlx)
+// {
+//     float x;
+//     float y;
+//     float z;
+//     float theta;
+//     int i;
+//     int row;
+//     int column;
+//
+//     clear_image(mlx->img);
+//     printf("render scale_factor: %f\n", mlx->map->scale_factor);
+//     i = 0;
+//     row = 0;
+//     column = 0;
+//
+//     float y_column;
+//
+//     float x_below;
+//     float y_below;
+//     float x2;
+//     float y2;
+//     // mlx->map->scale_factor = 1;
+//     while (i < mlx->map->width * mlx->map->height)
+//     {
+//         x = i % mlx->map->width;
+//         y = i / mlx->map->width;
+//         z = mlx->map->coords[i][0];
+//         y_column = y + 1;
+//
+//         // //scale image
+//         // x = x * mlx->map->scale_factor;
+//         // y = y * mlx->map->scale_factor;
+//         // z = z * mlx->map->scale_factor;
+//         // y_column = y_column * mlx->map->scale_factor;
+//
+//
+//
+//         //draw_line(mlx, x_coor(mlx, i), y_coor(mlx, i), x_coor(mlx, i + 1), y_coor(mlx, i + 1));
+//
+//
+//
+//         // // x = x_coor(mlx, i);
+//         // // y = y_coor(mlx, i);
+//         //
+//         // x = i % mlx->map->width;
+//         // y = i / mlx->map->width;
+//         // z = mlx->map->coords[i][0];
+//         // // z = mlx->map->coords[i][0];
+//         // // y_column = y + 1;
+//         //
+//         //
+//         // // x2 = x_coor(mlx, i + 1);
+//         // // y2 = y_coor(mlx, i + 1);
+//         // x2 = (i + 1) % mlx->map->width;
+//         // y2 = (i + 1) / mlx->map->width;
+//         //
+//         // // x_below = x_coor(mlx, i + mlx->map->width);
+//         // // y_below = y_coor(mlx, i + mlx->map->width);
+//         //
+//         // x_below = (i + mlx->map->width) % mlx->map->width;
+//         // y_below = (i + mlx->map->width) / mlx->map->width;
+//         //
+//         //
+//         // //scale image
+//         // x = x * mlx->map->scale_factor;
+//         // y = y * mlx->map->scale_factor;
+//         // z = z * mlx->map->scale_factor;
+//         // x2 = x2 * mlx->map->scale_factor;
+//         // y2 = y2 * mlx->map->scale_factor;
+//         // x_below = x_below * mlx->map->scale_factor;
+//         // y_below = y_below * mlx->map->scale_factor;
+//         //
+//         // //center
+//         // x = x + WIDTH / 2 - (mlx->map->width * mlx->map->scale_factor) /2;
+//         // x2 = x2 + WIDTH / 2 - (mlx->map->width * mlx->map->scale_factor) /2;
+//         // x_below = x_below + WIDTH / 2 - (mlx->map->width * mlx->map->scale_factor) /2;
+//         // y = HEIGHT / 2 + y;
+//         // y2 = HEIGHT / 2 + y2;
+//         // y_below = HEIGHT / 2 + y_below;
+//         //
+//         // //rotate_x;
+//         // theta = degrees_to_radians(90);
+//         // y = y * cos(theta) - z * sin(theta);
+//         // y2 = y2 * cos(theta) - z * sin(theta);
+//         // y_below = y_below * cos(theta) - z * sin(theta);
+//         //
+//         //
+//         // if (row < mlx->map->width && column < mlx->map->height)
+//         // {
+//         //     if (row != mlx->map->width - 1)
+//         //     {
+//         //         // draw_line(mlx, x_coor(mlx, i), y_coor(mlx, i), x_coor(mlx, i + 1), y_coor(mlx, i + 1));
+//         //
+//         //
+//         //         draw_line(mlx, x, y, x2, y2);
+//         //         // if (column != mlx->map->height - 1)
+//         //         //     draw_line(mlx, x_coor(mlx, i), y_coor(mlx, i), x_coor(mlx, i + mlx->map->width), y_coor(mlx, i + mlx->map->width));
+//         //         printf("x: %f y: %f \n", x_coor(mlx, i), y_coor(mlx,i));
+//         //     }
+//         //     if (column != mlx->map->height - 1)
+//         //     {
+//         //         draw_line(mlx, x, y, x_below, y_below);
+//         //     }
+//         //         // draw_line(mlx, x_coor(mlx, i), y_coor(mlx, i), x_coor(mlx, i + mlx->map->width), y_coor(mlx, i + mlx->map->width));
+//         //     // printf("x2: %f y2: %f \n", x , y + 1);
+//         //     row++;
+//         // }
+//         // else if (row == mlx->map->width)
+//         // {
+//         //     printf("\n");
+//         //     row = 0;
+//         //     column++;
+//         //     printf("%d\n", column);
+//         //     i--;
+//         // }
+//
+//
+//
+//
+//         float x_prev;
+//         float y_prev;
+//         if ((row < mlx->map->width) && y <= mlx->map->height)
+//         {
+//             //scale image
+//             x = x * mlx->map->scale_factor;
+//             y = y * mlx->map->scale_factor;
+//             z = z * mlx->map->scale_factor;
+//             y_column = y_column * mlx->map->scale_factor;
+//
+//             //rotate_x;
+//             theta = degrees_to_radians(90);
+//             y = y * cos(theta) - z * sin(theta);
+//             z = y * cos(theta) + z * sin(theta);
+//             y_column = y_column * cos(theta) - z * sin(theta);
+//
+//             x = x + WIDTH / 2 - (mlx->map->width * mlx->map->scale_factor) /2;
+//             y = HEIGHT / 2 + y;
+//             y_column = HEIGHT / 2 + y_column;
+//
+//             // draw_line(mlx, x_coor(mlx, i), y_coor(mlx, i), x_coor(mlx, i + 1), y_coor(mlx, i + 1));
+//             // printf("x: %f y: %f x_column: %f y_column: %f\n", x, y, x, y_column);
+//
+//             if (row > 0)
+//             {
+//                 draw_line(mlx, x_prev, y_prev, x , y);
+//                 if (column != mlx->map->height)
+//                     draw_line(mlx, x, y, x, y_column);
+//                 printf("x: %f y: %f x_column: %f y_column: %f\n", x, y, x, y_column);
+//                 x_prev = x;
+//                 y_prev = y;
+//             }
+//             else if (row == 0)
+//             {
+//                 x_prev = x;
+//                 y_prev = y;
+//
+//                 if (column != mlx->map->height - 1)
+//                     draw_line(mlx, x, y, x, y_column);
+//                 column++;
+//             }
+//             // if (column < mlx->map->height)
+//             //     draw_line(mlx, x, y, x, y_column);
+//             // printf("x: %f y: %f z: %f\n", x, y, z);
+//             // plot_pixel(mlx,x,y);
+//             row++;
+//         }
+//         if ((row == mlx->map->width) && (row = 0))
+//             i = i - 2;
+//
+//
+//         //
+//         // //scale image
+//         // x = x * mlx->map->scale_factor;
+//         // y = y * mlx->map->scale_factor;
+//         // z = z * mlx->map->scale_factor;
+//         //
+//         // //rotate_x;
+//         // theta = degrees_to_radians(90);
+//         // y = y * cos(theta) - z * sin(theta);
+//         // z = y * cos(theta) + z * sin(theta);
+//         //
+//         // //center
+//         // x = x + WIDTH / 2 - (mlx->map->width * mlx->map->scale_factor) /2;
+//         // y = HEIGHT / 2 + y;
+//         // // printf("x:%f y:%f ", x, y);
+//         // plot_pixel(mlx, x, y);
+//
+//         i++;
+//         printf("\n");
+//     }
+//     mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, mlx->img->img, 0 , 0);
+// }
 
 // void render(t_mlx *mlx)
 // {
